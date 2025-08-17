7d7d325fb34606ebd621399189d03b8d4435b10b
Ankit Panda <ankitpanda1612@gmail.com>
2025-08-17 23:25:46 +0530

    updated code

--- src/stories/DataTable.stories.tsx
+++ src/stories/DataTable.stories.tsx
@@ -1,5 +1,6 @@
-import { Meta, StoryObj } from "@storybook/react";
-import { DataTable, Column } from "../components/DataTable";
+import type { Meta, StoryObj } from "@storybook/react";
+import { DataTable } from "../components/DataTable";
+import type { Column } from "../components/DataTable";
 
 interface User {
   id: number;
